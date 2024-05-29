dc084750f02e3d691c0e3f8b2e94894ea71d889a
Indiara C. Menegat <indicm@gmail.com>
2024-05-29 00:46:00 -0300

    CreateTweetUseCase

---
+++ src/usecases/CreateTweetUseCase.ts
@@ -0,0 +1,20 @@
+import { HttpError } from 'common/errors/HttpError';
+import { BaseUseCase } from 'contracts/usecases/BaseUseCase';
+import { TweetService } from 'services/TweetService';
+
+export class CreateTweetUseCase implements BaseUseCase {
+  constructor(private tweetService: TweetService) {}
+
+  async execute(tweetData: any) {
+    const tweetExists = await this.tweetService.findById(
+      tweetData.id,
+    );
+    if (tweetExists) {
+      throw new HttpError({ status: 400, message: 'Tweet já existe.' });
+    }
+
+    const tweet = await this.tweetService.create(tweetData);
+
+    return tweet;
+  }
+}
