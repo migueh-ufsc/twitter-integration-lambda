e8bfd8793d54d6b2a5be2b99cb77ce6b9d596469
Indiara C. Menegat <indicm@gmail.com>
2024-11-28 23:51:21 -0300

    remove server e dependencias não usadas

--- src/infra/config.ts
+++ src/infra/config.ts
@@ -3,7 +3,6 @@ dotenv.config();
 
 export abstract class Config {
   static readonly mongoURI = process.env.MONGODB_URI;
-  static readonly rabbitURI = process.env.RABBITMQ_URI;
   static readonly serverPort = process.env.SERVER_PORT || 3000;
   static readonly logLevel = process.env.LOG_LEVEL || 'info';
   static readonly twitter = {
--- src/infra/server/Routes.ts
+++
@@ -1,7 +0,0 @@
-/** Arquivo pra cadastrar rotas caso exista um server */
-import { Express } from 'express';
-import { default as UserRouter } from './routes/UserRouter';
-
-export const initRoutes = (app: Express) => {
-  app.use('/user', UserRouter);
-};
